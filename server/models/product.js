52dfcc5c97cb75b1000e3a35b0ff5f0cc98b8655
Christopher James <68414004+Mindful-Developer@users.noreply.github.com>
2023-10-05 19:32:27 -0400

    Add category and product models

--- server/models/product.js
+++ server/models/product.js
@@ -20,7 +20,8 @@ const ProductSchema = new Schema({
         required: true
     },
     category: {
-        type: String
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
     }
 });
 
